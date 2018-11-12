3c3150e52c4a1dd2beebf0d3b82e05f14ff6619e
Joey Kuhn <jkuhn@stevens.edu>
2018-11-11 21:39:27 -0500

    Worked on help file

--- help.js
+++ help.js
@@ -1,11 +1,27 @@
+// Received following code from https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-22.php
+function remove_character(str, char_pos) 
+ {
+  part1 = str.substring(0, char_pos);
+  part2 = str.substring(char_pos + 1, str.length);
+  return (part1 + part2);
+ }
+ // Done
+
 // Cleans the input from a given string, returns the string
 function cleanInput (input) {
-
+    sLength = input.length;
+    for (var i = 0; i < sLength; i++) {
+        if ((input.charAt(i) >= 'A' && input.charAt(i) <= 'Z') || (input.charAt(i) >= 'a' && input.charAt(i) <= 'z'))
+        {
+            remove_character(input, i);
+        }
+      }
     return input;
 }
 
 // Cleans the input from a given file, returns a 2d array
-function cleanInputFromFile (path) {
+function cleanInputFromFile(path) {
+
 
     return array;
 }
